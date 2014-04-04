202fa98643fe36a23b1693fe08abd97900fcd759
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2014-04-04 14:34:23 -0400

    adding mex wrapper for treeapprox_greedy

---
+++ src/treeapprox_greedy_mex_wrapper.cc
@@ -0,0 +1,54 @@
+#include <vector>
+
+#include <math.h>
+#include <matrix.h>
+#include <mex.h>
+
+#include "mex_helper.h"
+#include "treeapprox_greedy.h"
+
+using std::vector;
+using treeapprox::treeapprox_greedy;
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+  if (nrhs != 3) {
+    mexErrMsgTxt("Exactly three input argument required (signal, tree degree,"
+        " k).");
+  }
+  if (nlhs > 1) {
+    mexErrMsgTxt("Too many output arguments.");
+  }
+
+  // required parameters
+
+  vector<double> x;
+  bool is_column_vector;
+  if (!get_double_vector(prhs[0], &x, &is_column_vector)) {
+    mexErrMsgTxt("Could not read the signal as a vector of type double.");
+  }
+
+  size_t d = 0;
+  if (!get_double_as_size_t(prhs[1], &d)) {
+    mexErrMsgTxt("Tree degree has to be a positive integer.");
+  }
+
+  size_t k = 0;
+  if (!get_double_as_size_t(prhs[2], &k)) {
+    mexErrMsgTxt("k to be a positive integer.");
+  }
+
+  // function call
+
+  vector<bool> support;
+  bool result = treeapprox_greedy(x, d, k, &support);
+
+  if (!result) {
+    mexErrMsgTxt("treeapprox_greedy returned false.");
+  }
+
+  // return values
+
+  if (nlhs >= 1) {
+    set_double_vector(&(plhs[0]), support, is_column_vector);
+  }
+}
